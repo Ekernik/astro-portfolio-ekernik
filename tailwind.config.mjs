e4fcd657fba50bf25435e35bde739e4e37712dce
Ekernik <ekernik@gmail.com>
2025-01-22 13:12:49 -0500

    feat: use custom color palete

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      orange: '#E57234',
+      egg: '#F7F7F7',
+      border: '#F5F5F5',
+    },
+    fontFamily: {
+      serif: ['Istok Web', 'serif'],
+    },
+  },
+  plugins: [],
+};
