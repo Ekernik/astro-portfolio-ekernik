7f4ed75fb8ee734644de3c5f3da16e15bdbdcaa4
Ekernik <ekernik@gmail.com>
2025-01-22 12:58:35 -0500

    feat: add Tech Stack section

---
+++ src/components/techStack.ts
@@ -0,0 +1,26 @@
+export const techStack = [
+  {
+    title: 'Frameworks',
+    techList: ['Next.js', 'Astro', 'HTMX'],
+  },
+  {
+    title: 'Libraries',
+    techList: ['React.js', 'AuthJS', 'next-intl'],
+  },
+  {
+    title: 'Styling',
+    techList: ['Shadcn/ui', 'TailwindCSS', 'SCSS Modules'],
+  },
+  {
+    title: 'Database and ORM',
+    techList: ['SQLite3', 'PostgreSQL', 'Prisma'],
+  },
+  {
+    title: 'State Management',
+    techList: ['Zustand', 'React Context', 'Redux'],
+  },
+  {
+    title: 'Queries',
+    techList: ['ReactQuery', 'GraphQL', 'Apollo Client'],
+  },
+];
