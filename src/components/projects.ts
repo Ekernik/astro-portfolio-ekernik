e7dbd75ae1aee147294bf47ad966fbed277075e9
Ekernik <ekernik@gmail.com>
2025-01-22 19:55:25 -0500

    refactor: decompose Projects component into smaller reusable chunks, add micro data

---
+++ src/components/projects.ts
@@ -0,0 +1,11 @@
+import heartteamThumbnail from '../assets/images/heartteam.png';
+
+export const projects = [
+  {
+    imageSrc: heartteamThumbnail.src,
+    title: 'Center of Cardiovascular Surgery and Interventional Cardiology',
+    description: 'Website for cardiology clinic.',
+    linkText: 'heartteamspb.com',
+    linkUrl: 'https://heartteamspb.com',
+  },
+];
