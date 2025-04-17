2097ec7a02c6cf7e896f186a2ac87b05a4305413
Ekernik <ekernik@gmail.com>
2025-04-17 16:13:48 -0400

    refactor: cloudflare configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   integrations: [react(), tailwind()],
-  adapter: cloudflare()
 });
